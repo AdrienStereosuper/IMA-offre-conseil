e3cfa68152968c65d78ba13ca62b7dca2d4a275f
Boris Forconi <boris@stereosuper.fr>
2014-06-19 18:00:16 +0200

    merge

--- js/script.js
+++ js/script.js
@@ -672,38 +672,21 @@ function readyMetiers(){
 	});
 	
 	// survol d'une puce métier
-	
-<<<<<<< HEAD
 	$("ul#puces-metiers li.puce-metier").hover(function(){
-		if(($("html").hasClass("no-touch"))&&($(window).width()>767)){
-		// au mouse enter
-		idPuceMetier = $(this).attr("id");
-		$(this).css('z-index',2);
-		animAppartitionBlocQuesion(idPuceMetier);
+		if(($("html").hasClass("no-touch"))&&($(window).width()>979)){
+			// au mouse enter
+			idPuceMetier = $(this).attr("id");
+			$(this).css('z-index',2);
+			animAppartitionBlocQuesion(idPuceMetier);
 		}
 	}, function(){
 		// au mouse leave
-		if(($("html").hasClass("no-touch"))&&($(window).width()>767)){
-		$(this).css('z-index','');
-		animDisparitionBlocQuesion();
+		if(($("html").hasClass("no-touch"))&&($(window).width()>979)){
+			$(this).css('z-index','');
+			animDisparitionBlocQuesion();
 		}
 	});
-=======
-		$("ul#puces-metiers li.puce-metier").hover(function(){
-			if(($("html").hasClass("no-touch"))&&($(window).width()>979)){
-				// au mouse enter
-				idPuceMetier = $(this).attr("id");
-				$(this).css('z-index',2);
-				animAppartitionBlocQuesion(idPuceMetier);
-			}
-		}, function(){
-			// au mouse leave
-			if(($("html").hasClass("no-touch"))&&($(window).width()>979)){
-				$(this).css('z-index','');
-				animDisparitionBlocQuesion();
-			}
-		});
->>>>>>> FETCH_HEAD
+
 	
 	
 	$("ul#puces-metiers li.puce-metier .bloc-bulle").click(function(){
